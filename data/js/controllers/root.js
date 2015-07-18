8d2f26bc0e1a0013a3576b28d6074586a0d740dd
Espen Henriksen <dev@esphen.com>
2015-07-18 17:06:48 +0200

    Alert box when request is already in collection

--- data/js/controllers/root.js
+++ data/js/controllers/root.js
@@ -2,8 +2,10 @@
 
 angular.module('RestedApp')
 .controller('RootCtl', function($scope) {
+
   // This is an array so we can later
-  // extend ability to add more collections
+  // extend app with the ability
+  // to add more collections.
   $scope.collections = [
     {
       name: 'Collection',
@@ -22,6 +24,15 @@ angular.module('RestedApp')
 
   // This is exposed to lower scopes
   $scope.addRequestToCollection = function(request) {
-    $scope.collections[0].requests.push(request);
+
+    if ($scope.collections[0].requests.indexOf(request) === -1) {
+      $scope.collections[0].requests.push(request);
+    } else {
+
+      // Todo: Replace this with a generic message
+      // modal that doesn't look like shit.
+      // (Looking at you, alert box)
+      alert('Request is already in collection');
+    }
   };
 });
