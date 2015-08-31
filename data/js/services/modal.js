a9fa70a445f419b78c85b88ffe7369e9ab7b914d
Espen Henriksen <espen.henriksen@me.com>
2015-08-31 20:56:18 +0200

    Add modal options documentation

--- data/js/services/modal.js
+++ data/js/services/modal.js
@@ -2,6 +2,19 @@
 
 angular.module('RestedApp')
 .factory('Modal', function($rootScope) {
+  /**
+   * Options:
+   * {
+   *   title: 'The title of the modal',
+   *   body: 'The body of the modal',
+   *   includeURL: 'An optional template to include inside the modal',
+   *   action: { // An optional action button
+   *     text: 'The action button text',
+   *     click: function action() {}
+   *   }
+   * }
+   *
+   */
   return {
     set: function(options) {
       $rootScope.modalOptions = options;
