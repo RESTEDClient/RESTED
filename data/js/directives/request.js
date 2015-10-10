4ff711cf0cd05b9e0dec2f99ff3390914cd149ab
Espen Henriksen <dev@esphen.com>
2015-10-10 23:06:16 +0200

    Properly handle minimize-button persisted to DB

--- data/js/directives/request.js
+++ data/js/directives/request.js
@@ -124,22 +124,11 @@ angular.module('RestedApp')
       };
 
       scope.toggleCollections = function() {
-        // Logic handled in css and ngHide
-        scope.$root.options.collectionsExpanded = !scope.$root.options.collectionsExpanded;
-
-        var isMinimized = scope.$root.options.collectionsExpanded;
-        scope.toggleCollectionsConfig = {
-          title: (isMinimized ? 'Show' : 'Hide') + ' collections',
-          classes: ['fa', (isMinimized ? 'fa-compress' : 'fa-expand')]
-        };
+        // Animation logic handled in css and ngHide
+        scope.$root.options.collectionsMinimized = !scope.$root.options.collectionsMinimized;
 
         // Persist to DB
         DB.options.set({name: 'options', options: scope.$root.options}).then(null, Modal.errorHandler);
-      };
-
-      scope.toggleCollectionsConfig = {
-        title: 'Hide collections',
-        classes: ['fa', 'fa-expand']
       };
 
       scope.addRequestConfig = {
