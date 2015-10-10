6d7b5ca235ef0aa75f7a099ed2c289e10a50f078
Espen Henriksen <dev@esphen.com>
2015-10-10 15:50:43 +0200

    Make firefox unit tests run in travis

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
