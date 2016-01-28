3cf471413e9bf8dc579f5636a0f86291351781e3
Espen Henriksen <dev@henriksen.is>
2016-01-28 22:42:33 +0100

    WebExtensions: Manifest and main script

---
+++ firefox/main.js
@@ -0,0 +1,7 @@
+console.log('HELLO!');
+
+/*browser.browserAction.onClicked.addListener(function(tab) {
+  console.log('touch me');
+  browser.tabs.create({'url': browser.extension.getURL('data/rested.html')});
+});*/
+
