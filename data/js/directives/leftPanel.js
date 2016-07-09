d40a4ff24bf1aef4465f11783f84cefe4939ca07
Espen Henriksen <dev@henriksen.is>
2016-07-09 15:55:25 +0200

    Persist left-panel selection in options

--- data/js/directives/leftPanel.js
+++ data/js/directives/leftPanel.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('RestedApp')
-.directive('leftPanel', [function() {
+.directive('leftPanel', ['$rootScope', function($rootScope) {
   return {
     restrict: 'E',
     templateUrl: 'views/directives/leftPanel.html',
     link: function(scope) {
       scope.setLeftPanel = function(tab) {
-        scope.activeTab = tab;
+        // Persist in db
+        $rootScope.setOption('activeTab', tab);
       };
     }
   };
