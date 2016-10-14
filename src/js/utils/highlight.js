a3d45ad083be2bf809c1496444c9680e302127c8
Espen Henriksen <dev@henriksen.is>
2016-10-15 00:36:33 +0200

    Use react-highlight and react-bootstrap

--- src/js/utils/highlight.js
+++
@@ -1,9 +0,0 @@
-export default function highlightAll(disableHighlighting) {
-  if (!disableHighlighting) {
-    // TODO jQuery. Kill it if possible
-    $('pre code').each((i, block) => {
-      hljs.highlightBlock(block);
-    });
-  }
-}
-
