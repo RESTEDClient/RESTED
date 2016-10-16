360fa68699ff9497a9faf043fb8b9af19b086b49
Espen Henriksen <dev@henriksen.is>
2016-10-16 03:06:09 +0200

    Change from rollup to webpack
    Working around issues like "process is not defined" and
    "module is not defined" with replaces, then stuff like having to
    manually specify exports in commonjs files I can live with, but
    when the new react rc totally broke the app and tests would not
    run without that rc, that got the better of me. Back to familiar
    waters with webpack.
    
    /rant

---
+++ dev/webpack/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    path: "./dist",
+    filename: "rested.js"
+  },
+  /* Note: Inline source maps are super slow in Firefox */
+  devtool: 'source-map',
+  eslint: {
+    failOnWarning: false,
+    failOnError: false
+  },
+  module: {
+     preLoaders: [{
+       test: /\.js$/,
+       loader: 'eslint',
+       exclude: /node_modules/,
+    }],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css",
+      },
+      {
+        test: /\.js/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
