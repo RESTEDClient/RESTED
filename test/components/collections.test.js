d312759505cb42a0633c7d26692471f570b5f345
Espen Henriksen <dev@henriksen.is>
2016-10-30 23:22:50 +0100

    Add tests for Collections

--- test/components/collections.test.js
+++ test/components/collections.test.js
@@ -1,14 +1,119 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import { DragDropContext } from 'react-dnd';
+import TestBackend from 'react-dnd-test-backend';
 
 /* eslint-disable import/no-unresolved */
 import Collections from 'components/Collections';
+import Collection from 'components/Collections/Collection';
+
+import makeStore from '../makeStore';
+import mockCollections from '../mock/collections.mock.js';
 
 jest.mock('react-dom');
 
-test('Collections renders correctly', () => {
-  const tree = renderer.create(
-    <Collections />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+/**
+ * Wraps a component into a DragDropContext that uses the TestBackend.
+ */
+function wrapInTestContext(DecoratedComponent, store = makeStore()) {
+  return DragDropContext(TestBackend)(
+    class TestContextContainer extends React.Component {
+      render() {
+        return (
+          <Provider store={store}>
+            <DecoratedComponent {...this.props} />
+          </Provider>
+        );
+      }
+    }
+  );
+}
+
+describe('Collections component', () => {
+  const store = makeStore({
+    collections: mockCollections,
+  });
+
+  it('should match the previous snapshot', () => {
+    const BoxedComponent = wrapInTestContext(Collections, store);
+    const tree = renderer.create(
+      <BoxedComponent />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render a "no requests" message when given no collections', () => {
+    const BoxedComponent = wrapInTestContext(Collections);
+    const tree = mount(
+      <BoxedComponent />
+    );
+
+    expect(tree.find('h5').prop('children')).toEqual('No collected requests. Add by pressing "plus" in the top right of the request panel.');
+  });
+
+  it('should render two collections when given a dataset with two collections', () => {
+    const BoxedComponent = wrapInTestContext(Collections, store);
+    const tree = mount(
+      <BoxedComponent />
+    );
+
+    expect(tree.find('Collection').length).toEqual(2);
+  });
 });
+
+describe('Collection component', () => {
+  const store = makeStore({
+    collections: mockCollections,
+  });
+
+  it('should match the previous snapshot', () => {
+    const BoxedComponent = wrapInTestContext(Collection);
+    const requests = mockCollections
+      .getIn(['collections', 0, 'requests'])
+      .toJS();
+
+    const tree = renderer.create(
+      <BoxedComponent
+        collectionIndex={0}
+        requests={requests}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  xit('should change the styles of dragged elements', () => {
+    const BoxedComponent = wrapInTestContext(Collection);
+    const requests = mockCollections
+      .getIn(['collections', 1, 'requests'])
+      .toJS();
+
+    const tree = mount(
+      <BoxedComponent
+        collectionIndex={1}
+        requests={requests}
+      />
+    );
+
+    // Obtain a reference to the backend
+    const backend = tree.instance().getManager().getBackend();
+
+    // Test that the background color is white
+    console.log('tree.find("Panel").debug()', tree.find('Panel').debug());
+    expect(tree.find('Panel').props.style.opacity).toEqual(1);
+
+    // Find the drag source ID and use it to simulate the dragging operation
+    const box = TestUtils.findRenderedComponentWithType(root, Box);
+    backend.simulateBeginDrag([box.getHandlerId()]);
+
+    // Verify that the element changed its color
+    div = TestUtils.findRenderedDOMComponentWithTag(root, 'div');
+    expect(div.props.style.opacity).toEqual(0.4);
+
+    // See other backend.simulate* methods for more!
+  });
+
+  xit('should have draggable requests');
+});
+
--- test/makeStore.js
+++ test/makeStore.js
@@ -1,8 +1,15 @@
 import { combineReducers, createStore } from 'redux';
 import { reducer as form } from 'redux-form';
 
+import { initialState as collectionInitialState } from '../src/store/collections/reducer';
+
 export default (state = {}) => createStore(
-  combineReducers({ form, request: () => ({}) }),
+  combineReducers({
+    // Mock all the reducers as no-op reducers
+    form,
+    collections: (initialState = collectionInitialState) => initialState,
+    request: (initialState = {}) => initialState,
+  }),
   state,
 );
 
