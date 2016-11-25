4b29a9c6e108bd7441205fd5db40dd475fe16b55
Espen Henriksen <dev@henriksen.is>
2016-11-25 20:27:39 +0100

    Disable highlighting for requests sized > 50KB

--- src/components/Response/index.js
+++ src/components/Response/index.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { Panel } from 'react-bootstrap';
+import { Panel, Alert } from 'react-bootstrap';
 import Highlight from 'react-highlight';
 
 import Loading from './Loading';
@@ -32,6 +32,10 @@ export function Response({ response, loading }) {
   if (!response) return null;
 
   const { method, url, headers, body } = response;
+  const contentLength = headers.find(header => (
+    header.name === 'content-length'
+  ));
+
   return (
     <Panel header={<Titlebar method={method} url={url} />}>
       <h3>
@@ -39,9 +43,24 @@ export function Response({ response, loading }) {
         <small>{response.statusText}</small>
       </h3>
       <Headers headers={headers} />
-      <Highlight>
-        {body}
-      </Highlight>
+      {!contentLength || Number(contentLength.value) < 50000
+        ? (
+          <Highlight>
+            {body}
+          </Highlight>
+        ) : (
+          <span>
+            <Alert bsStyle="warning">
+              The size of the response is greater than 50KB, syntax
+              highlighting has been disabled for performance reasons.
+            </Alert>
+
+            <code><pre>
+              { body }
+            </pre></code>
+          </span>
+        )
+      }
     </Panel>
   );
 }
