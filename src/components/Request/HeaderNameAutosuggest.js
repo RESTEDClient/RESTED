4f0653f1ef0b1676664c2c19d0cbd8cabeb16df2
Espen Henriksen <dev@henriksen.is>
2017-09-13 21:23:59 +0200

    #94 Add fuzzy search for headers suggest

--- src/components/Request/HeaderNameAutosuggest.js
+++ src/components/Request/HeaderNameAutosuggest.js
@@ -1,29 +1,39 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import Autosuggest from 'react-autosuggest';
+import fuzzysort from 'fuzzysort';
 
 import headers from 'constants/commonHeaders';
 import { SuggestWrapper } from './StyledComponents';
 
+// Make search indexes ahead of time to go fast!
+const preparedHeaders = headers.map(header => fuzzysort.prepare(header));
 const maxEntries = 7;
+
+const compare = (a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+};
 
 // Calculate suggestions for any given input value.
 const getSuggestions = value => {
-  const inputValue = value.trim().toLowerCase();
+  const inputValue = value.trim();
   const inputLength = inputValue.length;
 
-  return inputLength === 0
-    ? []
-    : headers
-      .filter(header =>
-        header.toLowerCase().includes(inputValue),
-      )
-      .slice(0, maxEntries);
+  if (inputLength === 0) return [];
+
+  const matches = fuzzysort
+    .go(inputValue, preparedHeaders)
+    .sort((a, b) => compare(a.score, b.score));
+
+  return matches.slice(0, maxEntries);
 };
 
 // Teach Autosuggest how to calculate the input value for
 // every given suggestion.
-const getSuggestionValue = suggestion => suggestion;
+// eslint-disable-next-line no-underscore-dangle
+const getSuggestionValue = suggestion => suggestion._target;
 
 // Ensure the user does not send a request when selecting using enter
 function preventDefaultOnEnter(event, { method }) {
@@ -33,9 +43,9 @@ function preventDefaultOnEnter(event, { method }) {
 }
 
 const renderSuggestion = suggestion => (
-  <div>
-    {suggestion}
-  </div>
+  // This unsafe is fine, as it is from a trusted source
+  // eslint-disable-next-line react/no-danger
+  <div dangerouslySetInnerHTML={{ __html: suggestion.highlighted }} />
 );
 
 const renderInputComponent = ({ className, ...rest }) => (
