7088e53a33b15494c8729d45cadd9f9391f81d0c
Espen Henriksen <dev@henriksen.is>
2017-09-16 13:01:43 +0200

    #114 Parse JSON values as JSON types when possible

--- src/utils/buildRequestData.js
+++ src/utils/buildRequestData.js
@@ -1,5 +1,27 @@
 import { reMapHeaders } from 'utils/requestUtils';
 
+/**
+ * JSON values should be typed as their respective JSON types
+ * when possible, instead of always being strings.
+ */
+const parseValues = object => {
+  const result = object;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in result) {
+    if ({}.hasOwnProperty.call(result, key)) {
+      const value = result[key];
+      try {
+        result[key] = JSON.parse(value);
+      } catch (e) {
+        // no-op
+      }
+    }
+  }
+
+  return result;
+};
+
 export default function buildRequestData(bodyType, formData) {
   switch (bodyType) {
     case 'multipart':
@@ -31,7 +53,8 @@ export default function buildRequestData(bodyType, formData) {
 
     case 'json':
       if (formData && formData.filter(f => f.name).length > 0) {
-        const body = reMapHeaders(formData);
+        let body = reMapHeaders(formData);
+        body = parseValues(body);
 
         return JSON.stringify(body);
       }
