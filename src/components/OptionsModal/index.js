cf383d0d1f31c440d82486530c616ff0f5076072
ChiefOfGxBxL <langgb@clarkson.edu>
2021-11-29 17:27:27 -0500

    Add icons to options tabs

--- src/components/OptionsModal/index.js
+++ src/components/OptionsModal/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
 
+import Fonticon from 'components/Fonticon';
+
 import TemplateOptionsPane from './TemplateOptionsPane';
 import GeneralOptionsPane from './GeneralOptionsPane';
 import SyncPane from './SyncPane';
 import ImportPane from './ImportPane';
 import ExportPane from './ExportPane';
-
-import Fonticon from 'components/Fonticon';
 
 export default class OptionsModalBody extends React.Component {
   constructor(props) {
@@ -25,44 +25,52 @@ export default class OptionsModalBody extends React.Component {
         onSelect={activeTab => this.setState({ activeTab })}
         id="OptionTabs"
       >
-        <Tab eventKey={0} title={
-          <span>
+        <Tab
+          eventKey={0} title={<span>
             <Fonticon icon="link" />
             <span>URL Templates</span>
-          </span>
-        }>
+          </span>}
+        >
           <TemplateOptionsPane />
         </Tab>
-        <Tab eventKey={1} title={
-          <span>
-            <Fonticon icon="cogs" />
-            <span>Options</span>
-          </span>
-        }>
+        <Tab
+          eventKey={1} title={
+            <span>
+              <Fonticon icon="cogs" />
+              <span>Options</span>
+            </span>
+        }
+        >
           <GeneralOptionsPane />
         </Tab>
-        <Tab eventKey={2} title={
-          <span>
-            <Fonticon icon="refresh" />
-            <span>Sync</span>
-          </span>
-        }>
+        <Tab
+          eventKey={2} title={
+            <span>
+              <Fonticon icon="refresh" />
+              <span>Sync</span>
+            </span>
+        }
+        >
           <SyncPane />
         </Tab>
-        <Tab eventKey={3} title={
-          <span>
-            <Fonticon icon="download" />
-            <span>Import data</span>
-          </span>
-        }>
+        <Tab
+          eventKey={3} title={
+            <span>
+              <Fonticon icon="download" />
+              <span>Import data</span>
+            </span>
+        }
+        >
           <ImportPane />
         </Tab>
-        <Tab eventKey={4} title={
-          <span>
-            <Fonticon icon="upload" />
-            <span>Export data</span>
-          </span>
-        }>
+        <Tab
+          eventKey={4} title={
+            <span>
+              <Fonticon icon="upload" />
+              <span>Export data</span>
+            </span>
+        }
+        >
           <ExportPane />
         </Tab>
       </Tabs>
